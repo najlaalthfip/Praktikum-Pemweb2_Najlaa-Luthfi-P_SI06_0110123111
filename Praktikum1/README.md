494fcfac87c387cf8584292b981cb790335a7bab
najlaalthfip <146152413+najlaalthfip@users.noreply.github.com>
2024-03-04 14:52:09 +0700

    Add files via upload

---
+++ Praktikum1/README.md
@@ -0,0 +1,2 @@
+# Praktikum-Pemweb2_Najlaa-Luthfi-P_SI06_0110123111
+# Praktikum-Pemweb2_Najlaa_SI06_0110123111
---
+++ Praktikum1/array_multidimensi.php
@@ -0,0 +1,11 @@
+<?php
+//Deklarasi array multidimensi
+$mahasiswa = [
+    ["Ziyad", "Teknik Informatika", "Depok"],
+    ["Najlaa", "Sistem Informasi", "Jakarta"],
+    ["Tina", "Perbankan", "Depok"],
+    ["Sisil", "Akuntansi", "Pamulang"]
+];
+//Menampilkan data isi array 
+echo $mahasiswa[0][2];
+?>
